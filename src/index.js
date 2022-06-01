92cb84f2f1340302b2787915fdf48ca68d558752
Zentality <jmcp@live.com.au>
2022-06-01 16:40:57 +0800

    Create function to get weather data using location

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-const test = document.querySelector("h1");
-test.textContent = "Test linking of content";
+async function getWeatherInfo(location) {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=5486debd66620876ff108c5a748811d1`, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    })
+}
