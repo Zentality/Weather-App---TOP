8d5f86ff074a32a49af92266b9f28ddaf80fc182
Zentality <jmcp@live.com.au>
2022-06-03 16:42:38 +0800

    Put city and country on page using information from response

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const countryCodes = require('country-codes-list');
+
 async function getWeather(location) {
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=5486debd66620876ff108c5a748811d1`, { mode: 'cors' })
     .then((response) => response.json())
@@ -8,13 +10,28 @@ async function getWeather(location) {
     })
 }
 
+const element = (() => {
+  const form = document.querySelector("form");
+  const formInput = document.querySelector("#query");
+  const city = document.querySelector(".city");
+  const country = document.querySelector(".country");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    getWeather(formInput.value);
+    formInput.value = "";
+  })
+
+  return { city, country }
+})();
+
 function loadWeatherData(response) {
   console.log(response);
+  element.city.textContent = `${response.name}, `;
+  element.country.textContent = getCountryFromCode(response.sys.country);
 }
 
-const form = document.querySelector("form");
-const formInput = document.querySelector("#query");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  getWeather(formInput.value);
-})
+function getCountryFromCode(code) {
+  const countryCodeObject = countryCodes.customList('countryCode', '{countryNameEn}');
+  return countryCodeObject[code];
+}
