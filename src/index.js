60de6740625cc33255990914284dd4012910d0fb
Zentality <jmcp@live.com.au>
2022-06-05 11:31:03 +0800

    Display 7 days following current date in timezone

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ const element = (() => {
   const country = document.querySelector(".country");
   const temperatureNow = document.querySelector(".temperatureNow");
   const feelsLike = document.querySelector(".feelsLike");
+  const forecastDays = document.querySelectorAll(".dayOfWeek");
+  const forecastSymbols = document.querySelectorAll(".weatherSymbol");
+  const forecastTemps = document.querySelectorAll(".tempRange");
+  const forecastDateRange = document.querySelector(".forecastDateRange");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -29,33 +33,62 @@ const element = (() => {
     formInput.value = "";
   })
 
-  return { city, country, temperatureNow, feelsLike }
+  return { city, country, temperatureNow, feelsLike, forecastDays, forecastSymbols, forecastTemps }
 })();
 
 async function loadWeatherLocation(response) {
-  console.log(response);
-
   element.city.textContent = `${response[0].name}, `;
   element.country.textContent = getCountryFromCode(response[0].country);
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${response[0].lat}&lon=${response[0].lon}&appid=5486debd66620876ff108c5a748811d1`, { mode: 'cors' })
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${response[0].lat}&lon=${response[0].lon}&exclude=minutely,hourly,alerts&appid=5486debd66620876ff108c5a748811d1`, { mode: 'cors' })
     .then((response) => response.json())
     .then((response) => {
-      loadCurrentWeatherData(response);
+      loadWeatherData(response);
     }).catch((err) => {
       console.log(err);
     })
 }
 
-function loadCurrentWeatherData(response) {
-  console.log(response);
-  element.temperatureNow.textContent = getTempFromK(config.isFahrenheit, response.main.temp);
-  element.feelsLike.textContent = getTempFromK(config.isFahrenheit, response.main.feels_like);
+function loadWeatherData(response) {
+  console.log(response)
+  loadCurrentWeatherData(response.current.temp, response.current.feels_like);
+  loadForecast(response.daily, response.timezone_offset);
+}
+
+function loadCurrentWeatherData(tempInK, feelsLikeInK) {
+  element.temperatureNow.textContent = getTempFromK(config.isFahrenheit, tempInK);
+  element.feelsLike.textContent = getTempFromK(config.isFahrenheit, feelsLikeInK);
+}
+
+function loadForecast(forecastData, timezoneOffset) {
+  console.log(forecastData);
+  console.log(timezoneOffset);
+  for (let i = 0; i < 8; i++) {
+    element.forecastDays[i].textContent = getDateFromUnixTime(forecastData[i].dt + timezoneOffset);
+  }
 }
 
 function getCountryFromCode(code) {
   const countryCodeObject = countryCodes.customList('countryCode', '{countryNameEn}');
   return countryCodeObject[code];
+}
+
+function getDateFromUnixTime(timestamp) {
+  console.log(timestamp);
+  const date = new Date(timestamp * 1000);
+  const dayOfWeek = convertIndexToDay()
+  function convertIndexToDay() {
+    switch (date.getDay()) {
+      case 0: return "Sunday";
+      case 1: return "Monday";
+      case 2: return "Tuesday";
+      case 3: return "Wednesday";
+      case 4: return "Thursday";
+      case 5: return "Friday";
+      case 6: return "Saturday";
+    }
+  }
+  return (`${dayOfWeek}`)
 }
 
 function getTempFromK(isFahrenheit, kelvinValue) {
